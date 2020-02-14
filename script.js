67607abe8cb5bb52b65c7f2abf68dc71c0d566f0
DelrioRamirez4321 <delrioramirez7813@gmail.com>
2020-02-14 11:25:43 -0800

    finnished question 2

--- script.js
+++ script.js
@@ -2,8 +2,7 @@
 // containing all the numbers from start up to (and including) end.
 // Example output:
 // console.log(range(1, 10)); // outputs [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-let range = function(start,end) {
+function range(start,end) {
     let myArray = [];
     for (i = start; i <= end; i++) {
             myArray.push(i);
@@ -11,20 +10,20 @@ let range = function(start,end) {
     return myArray;
 };
 
-console.log(range(5,100));
-
-
-
-
+console.log(range(1,10));
 
 // 2. Next, write a sum function that takes an array of numbers and returns the sum of
 // these numbers. Run the example program and see whether it does indeed return 55.
 // Example output:
 // console.log(sum(range(1, 10))); // returns 55
-let sum = function() {
-    
+function sum(range) {
+    let total = 0;
+    for (let i in range) {
+     total = total + range[i];
+    }
+    return total;
 }
-console.log(sum(range(1, 10))); 
+console.log(sum(range(1, 10)))
 // 3. Arrays have a reverse method that changes the array by inverting the order in which
 // its elements appear. For this exercise, write two functions, reverseArray and
 // reverseArrayInPlace. The first, reverseArray, takes an array as an argument and
